56f2bbb4098f387b135f9ef1ff8c118b05bf0d71
Chris <cwaites@hotmail.com>
2020-10-02 21:04:28 +1000

    Eddy can move

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    float horMove = 0f;
+
+    bool jump = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horMove));
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
